e0c30c4a5d3b7b6b6613da039cb32c97f7256854
jpalmero9 <jpalmero9@gmail.com>
2024-01-14 11:28:27 -0500

    Se crea primer proyecto y se actualiza repositorio

---
+++ CaminoFullStack/SintaxisBasica/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SintaxisBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parte I
+            //Curso Básico (Sintaxis básica)
+
+            //Console.WriteLine("Hello, World!");
+
+            int x = 5;
+            int y = 3;
+            int result = x > y ? x : y;
+            Console.WriteLine(result);
+
+
+
+            //Variables
+            //modificador acceso + tipo de dato + nombreVariable
+
+            Console.WriteLine("las Variables");
+
+
+            //Constantes
+            Console.WriteLine("Las Constante");
+
+
+
+
+            //Tipo null
+            
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
